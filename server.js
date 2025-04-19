675e850d39c1a836a40c74b1fd4a663bd26cbe8a
SHARIQ HUSSAIN <shariqhussain691@gmail.com>
2025-04-19 20:01:36 +0530

    cors added

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const contactRoute = require("./routes/contact");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const allowedOrigins = ["https://jade-torrone-fb6ab1.netlify.app"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use("/api/contact", contactRoute);
 
 const PORT = process.env.PORT || 5000;
